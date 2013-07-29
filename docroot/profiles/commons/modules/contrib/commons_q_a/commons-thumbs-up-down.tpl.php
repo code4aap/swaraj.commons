8d2efc972bd4f06c58173a9a7a81bb0125eea16a
Akash Jain <akash@akash-Lenovo-G580.(none)>
2013-07-29 19:25:35 +0530

    user points only visible to administrator

--- docroot/profiles/commons/modules/contrib/commons_q_a/commons-thumbs-up-down.tpl.php
+++ docroot/profiles/commons/modules/contrib/commons_q_a/commons-thumbs-up-down.tpl.php
@@ -7,8 +7,11 @@
 
 <div class="commons-q-a-rate-up-down clearfix">
   <?php
-  print '<div class="rate-info"><span class="rate-info-value">' . $results['rating'] . '</span> <span class="rate-info-label">' . format_plural($results['rating'], 'point', 'points') . '</span></div>';
-
+  
+  global $user;
+  if (in_array("administrator",$user->roles))  {
+   print '<div class="rate-info"><span class="rate-info-value">' . $results['rating'] . '</span> <span class="rate-info-label">' . format_plural($results['rating'], 'point', 'points') . '</span></div>';
+}
   if ($display_options['description']) {
     print '<div class="rate-description">' . $display_options['description'] . '</div>';
   }
@@ -16,10 +19,10 @@
 
   <div class="commons-q-a-rate-buttons">
     <div class="commons-q-a-rate-trigger commons-q-a-rate-up">
-      <?php print $up_button; ?>
+      <?php if (in_array("administrator",$user->roles))  {print $up_button;} ?>
     </div>
     <div class="commons-q-a-rate-trigger commons-q-a-rate-down">
-      <?php print $down_button; ?>
+      <?php  if (in_array("administrator",$user->roles))  {print $down_button;} ?>
     </div>
   </div>
 </div>
